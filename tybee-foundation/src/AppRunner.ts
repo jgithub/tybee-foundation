c23337d3f515c610ce2d7a22b2e1536fb4163f3c
jgithub <jgithub1@jeano.net>
2024-08-18 17:59:01 -0500

    ExpressJS + Healthcheck

--- tybee-foundation/src/AppRunner.ts
+++ tybee-foundation/src/AppRunner.ts
@@ -2,16 +2,32 @@ import 'dotenv/config'
 import { getLogger, d4l, DateProviderService } from '@jgithub/ts-gist-pile';
 const LOG = getLogger(`AppRunner`)
 import * as dotenv from "dotenv";
-dotenv.config()
 import container from './inversify.config'
 import DI_TYPES from "./diTypes";
+import express, { Express, Request, Response } from "express";
+dotenv.config()
 
 
 export class AppRunner {
   public async run(): Promise<boolean> {
+
     LOG.debug(`run(): Entering.  NODE_ENV = ${d4l(process.env.NODE_ENV)},  cwd = ${process.cwd()}`)
     const dateProviderService = container.get<DateProviderService>(DI_TYPES.DateProviderService);
     LOG.debug(`run(): Time at the tone is ${d4l(dateProviderService.getNow())}`)
+
+
+    const app: Express = express();
+    const port = process.env.PORT || 3000;
+    
+
+    app.get("/healthcheck", (req: Request, res: Response) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ message: 'ok'}));
+    });
+
+    app.listen(port, () => {
+      console.log(`[server]: Server is running at http://localhost:${port}`);
+    });
 
     return true;
   }

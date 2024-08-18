b8d51229560932480ce97f49aeeff10fe380f4bc
jgithub <jgithub1@jeano.net>
2024-08-18 16:55:33 -0500

    towards eslint

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+// eslint.config.js
+
+// https://typescript-eslint.io/
+// https://eslint.org/docs/latest/use/configure/configuration-files#using-predefined-configurations
+// https://eslint.org/docs/latest/use/configure/migration-guide#importing-plugins-and-custom-parsers
+// https://eslint.org/docs/latest/use/configure/migration-guide#using-eslintrc-configs-in-flat-config
+// https://eslint.org/docs/latest/use/configure/migration-guide#predefined-and-shareable-configs
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+);

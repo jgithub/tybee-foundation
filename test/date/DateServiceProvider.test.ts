cfdd88f53e9f97cced6439eaab6680357fc6416f
jgithub <jgithub1@jeano.net>
2024-08-18 17:16:47 -0500

    towards eslint

--- test/date/DateServiceProvider.test.ts
+++ test/date/DateServiceProvider.test.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import { getLogger, d4l, DateProviderService } from '@jgithub/ts-gist-pile';
-
+import container from '../../src/inversify.config'
+import DI_TYPES from "../../src/diTypes";
 
 describe('DateProviderService', () => {
   it('has the expected interface', () => {
@@ -10,5 +11,10 @@ describe('DateProviderService', () => {
       }
     }
     expect(dateProviderService.getNow().toISOString()).to.eql("1970-01-01T00:00:00.000Z")
+  }) 
+  
+  it('exists as a singleton service in dependency injection', () => {
+    const dateProviderService: DateProviderService = container.get<DateProviderService>(DI_TYPES.DateProviderService);
+    expect(dateProviderService.getNow().getTime()).to.be.greaterThan(1724019367551)
   })  
 })

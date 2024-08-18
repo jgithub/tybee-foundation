6efed248a38bb01e7d9d90426e75aceca88acdad
jgithub <jgithub1@jeano.net>
2024-08-18 13:26:59 -0500

    Inversify is working

--- src/AppRunner.ts
+++ src/AppRunner.ts
@@ -1,6 +1,5 @@
 import 'dotenv/config'
-import { d4l } from '@jgithub/ts-gist-pile';
-import { getLogger } from '@jgithub/ts-gist-pile';
+import { getLogger, d4l, DateProviderService } from '@jgithub/ts-gist-pile';
 const LOG = getLogger(`AppRunner`)
 import * as dotenv from "dotenv";
 dotenv.config()
@@ -13,6 +12,9 @@ import DI_TYPES from "./diTypes";
 export class AppRunner {
   public async run(): Promise<boolean> {
     LOG.debug(`run(): Entering.  NODE_ENV = ${d4l(process.env.NODE_ENV)},  cwd = ${process.cwd()}`)
+    const dateProviderService = container.get<DateProviderService>(DI_TYPES.DateProviderService);
+    LOG.debug(`run(): Time at the tone is ${d4l(dateProviderService.getNow())}`)
+
     return true;
   }
 }

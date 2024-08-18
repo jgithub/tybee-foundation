77a0d18d89a73a9c00429f8e2a6a2326f0c53a9a
jgithub <jgithub1@jeano.net>
2024-08-18 17:39:01 -0500

    Another tybee-foundation root folder

--- tybee-foundation/eslint.config.js
+++ tybee-foundation/eslint.config.js
@@ -6,44 +6,6 @@
 // https://eslint.org/docs/latest/use/configure/migration-guide#using-eslintrc-configs-in-flat-config
 // https://eslint.org/docs/latest/use/configure/migration-guide#predefined-and-shareable-configs
 
-// export default [{
-  // languageOptions: {
-  //   ecmaVersion: "latest",
-  //   // parserOptions: {
-  //   //   ecmaVersion: 2024, // or higher, depending on your JavaScript version
-  //   //   sourceType: 'module', // if using modules
-  //   // },
-  //   sourceType: "module",
-  //   parser: "@typescript-eslint/parser"
-  // },
-  // plugins: {
-  //   "@typescript-eslint": {}
-  // },
-  // extends: [
-  //   "eslint:recommended",
-  //   "plugin:@typescript-eslint/eslint-recommended",
-  //   "plugin:@typescript-eslint/recommended"
-  // ],
-  // ecmaVersion: "latest",
-  // files: ["**/*.ts"],
-  // rules: {
-  //   "no-unused-vars": "error",
-  //   "no-undef": "error",
-  //   "eqeqeq": "error",
-  //   "no-irregular-whitespace": "error",
-  //   "indent": ['error', 2]
-  // }
-// }];
-
-
-// eslint.config.js
-
-// https://typescript-eslint.io/
-// https://eslint.org/docs/latest/use/configure/configuration-files#using-predefined-configurations
-// https://eslint.org/docs/latest/use/configure/migration-guide#importing-plugins-and-custom-parsers
-// https://eslint.org/docs/latest/use/configure/migration-guide#using-eslintrc-configs-in-flat-config
-// https://eslint.org/docs/latest/use/configure/migration-guide#predefined-and-shareable-configs
-
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import globals from "globals";
@@ -57,8 +19,6 @@ export default tseslint.config(
         ...globals.node
       }
     },
-    // ...tseslint.configs.recommended,
-    // ecmaVersion: "latest",
     files: ["**/*.ts"],
     rules: {
       "no-unused-vars": "error",

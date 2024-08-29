4001c0b748ae95c741bf4f6d17a21c332e2153ff
JV <jgithub@users.noreply.github.com>
2024-08-29 13:30:55 -0500

    media.start cleanup

--- migration/000002_seed.up.sql
+++ migration/000002_seed.up.sql
@@ -15,13 +15,13 @@ INSERT INTO entity
 
 
 INSERT INTO qa_question 
-(uuid, phrase, sequence, audio_file) VALUES ('1ef64a85-9a94-6450-8828-10638dbfcd68', 'What is your name?', 3, 'what_is_your_favorite_color.wav');
+(uuid, phrase, sequence, audio_file) VALUES ('1ef64a85-9a94-6450-8828-10638dbfcd68', 'What is your name?', 3, '3.mp3');
 
 INSERT INTO qa_question 
-(uuid, phrase, sequence, audio_file) VALUES ('1ef656ad-d408-6fa0-b1cc-3e00db43cf8f', 'What is your quest?', 2, 'what-is-your-quest.m4a');
+(uuid, phrase, sequence, audio_file) VALUES ('1ef656ad-d408-6fa0-b1cc-3e00db43cf8f', 'What is your quest?', 2, '2.mp3');
 
 INSERT INTO qa_question 
-(uuid, phrase, sequence, audio_file) VALUES ('1ef64a86-9a94-6450-8828-10638dbfcd68', 'What is your favorite color?', 1, 'what_is_your_favorite_color.wav');
+(uuid, phrase, sequence, audio_file) VALUES ('1ef64a86-9a94-6450-8828-10638dbfcd68', 'What is your favorite color?', 1, '1.mp3');
 
 INSERT INTO qa_question 
-(uuid, phrase, sequence, audio_file) VALUES ('1ef66123-d408-6fa0-b1cc-3e00db43cf8f', 'What... is the capital of Assyria? ', 4, 'demo.mp3');
+(uuid, phrase, sequence, audio_file) VALUES ('1ef66123-d408-6fa0-b1cc-3e00db43cf8f', 'What... is the capital of Assyria? ', 4, '4.mp3');
